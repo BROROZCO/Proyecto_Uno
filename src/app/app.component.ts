ccef380edacee69fdbf2709904d8cc2560355e24
unknown <unenes.viejos@gmail.com>
2020-12-17 20:14:42 -0600

    Uso del *ngIf, validando registros en la tabla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent {
     { nombre: "Anyel Ramirez", estado: "Regular", id: 17 },
     { nombre: "Flor Garcia", estado: "Activo", id: 78 },
     { nombre: "Anahis Aguilera", estado: "Regular", id: 78 },
-    { nombre: "Antonio Cuadra", estado: "Activo", id: 78 }
+    { nombre: "Antonio Cuadra", estado: "Activo", id: 78 } 
   ]
+
+  mostrar = true;
+
+  toogle(): void{
+    this.mostrar = !this.mostrar
+  }
 }
